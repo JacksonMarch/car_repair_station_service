e5958133e52e3c1418b751b076dad982853e76f5
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-07-13 22:06:09 +0300

    Stylistic errors corrected(long lines)

--- autoservice/admin.py
+++ autoservice/admin.py
@@ -1,18 +1,47 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import ServiceAdvisor, Order, Technician, OrderType, MasterQualification
+from .models import (
+    Order,
+    OrderType,
+    Technician,
+    ServiceAdvisor,
+    MasterQualification
+)
 
 
 @admin.register(ServiceAdvisor)
 class ServiceAdvisorAdmin(UserAdmin):
-    list_display = ("username", "first_name", "last_name", "is_staff")
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff"
+    )
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("id", "client_full_name", "car_model", "car_year", "order_type", "service_advisor", "created_at")
-    search_fields = ("client_full_name", "client_number", "car_model", "complaint")
-    list_filter = ("order_type", "service_advisor", "technician", "created_at")
+    list_display = (
+        "id",
+        "client_full_name",
+        "car_model",
+        "car_year",
+        "order_type",
+        "service_advisor",
+        "created_at"
+    )
+    search_fields = (
+        "client_full_name",
+        "client_number",
+        "car_model",
+        "complaint"
+    )
+    list_filter = (
+        "order_type",
+        "service_advisor",
+        "technician",
+        "created_at"
+    )
     date_hierarchy = "created_at"
 
 
