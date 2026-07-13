51ea982da26543e108e1fc04647c58095f2cbdd4
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-07-13 22:00:13 +0300

    Add static directory with styles.css file and admin panel configured. Added filters and search field for OrderAdmin, also changed display for OrderAdmin and ServiceAdvisorAdmin.

--- autoservice/admin.py
+++ autoservice/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import ServiceAdvisor, Order, Technician, OrderType, MasterQualification
 
-# Register your models here.
+
+@admin.register(ServiceAdvisor)
+class ServiceAdvisorAdmin(UserAdmin):
+    list_display = ("username", "first_name", "last_name", "is_staff")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "client_full_name", "car_model", "car_year", "order_type", "service_advisor", "created_at")
+    search_fields = ("client_full_name", "client_number", "car_model", "complaint")
+    list_filter = ("order_type", "service_advisor", "technician", "created_at")
+    date_hierarchy = "created_at"
+
+
+admin.site.register(Technician)
+admin.site.register(OrderType)
+admin.site.register(MasterQualification)
