e5dd346ac81a5546a3110bb19f0e899697a40fee
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-07-23 18:18:23 +0300

    :update settings & requirements.txt

--- car_repair_station_service/settings.py
+++ car_repair_station_service/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -123,3 +123,5 @@ AUTH_USER_MODEL = "autoservice.ServiceAdvisor"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+LOGIN_REDIRECT_URL = "/"
