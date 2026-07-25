4c349f98761b9c63f6a62c4b820fd2c6db681486
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-07-25 21:50:14 +0300

    Fix some bugs for Flake8

--- car_repair_station_service/settings/prod.py
+++ car_repair_station_service/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
@@ -17,7 +17,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSSWORD"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
         "HOST": os.environ["POSTGRES_HOST"],
         "PORT": int(os.environ["POSTGRES_DB_PORT"]),
     }
